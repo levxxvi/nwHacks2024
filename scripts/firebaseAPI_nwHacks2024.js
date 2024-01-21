e3bdc9721b84e5db933f4b12494f612ac2e6ef18
Alexandra Wong <lex115wong@gmail.com>
2024-01-20 20:07:25 -0800

    Fixed sign in

--- scripts/firebaseAPI_nwHacks2024.js
+++ scripts/firebaseAPI_nwHacks2024.js
@@ -1,6 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +14,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+// // const app = initializeApp(firebaseConfig);
+// const db = firebase.firestore();
+// // const analytics = getAnalytics(app);
+
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const analytics = getAnalytics(app);
